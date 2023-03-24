9e7bf6fe2b654ad010a99c11214bb04f545ef350
jutionck <jutionck@gmail.com>
2023-03-24 14:07:04 +0700

    add: accessing db

--- app.go
+++ app.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
-	fmt.Println("Getting Started")
+	fmt.Println("Accessing Database")
+	// Koneksi DB
+	host := "localhost"
+	user := "postgres"
+	password := "P@ssw0rd"
+	database := "db_sinar_harapan_makmur_v2"
+	port := "5432"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, database, port)
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := conn.DB()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
