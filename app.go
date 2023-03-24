7f035db9938d5703132a530aa000ac8599c03636
jutionck <jutionck@gmail.com>
2023-03-24 14:06:09 +0700

    update: getting started

--- app.go
+++ app.go
@@ -2,63 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
-
-// Declare Model
-// Conventions
-// -> Jika di struct fieldny ProductionYear maka otomatis di db production_year
-// -> Untuk nama tabel akan di convert ke dalam plural, vehicle -> vehicles
-type Vehicle struct {
-	Brand          string
-	Model          string
-	ProductionYear int
-	Color          string
-	IsAutomatic    bool
-	Stock          int
-	SalePrice      int
-	Status         string // enum: "Baru" & "Bekas"
-	BaseModel
-}
-
-type BaseModel struct {
-	ID        string `gorm:"primaryKey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-}
-
-func (v *Vehicle) IsValidStatus() bool {
-	return v.Status == "Baru" || v.Status == "Bekas"
-}
 
 func main() {
 	fmt.Println("Getting Started")
-
-	// Koneksi DB
-	host := "localhost"
-	user := "postgres"
-	password := "P@ssw0rd"
-	database := "db_sinar_harapan_makmur_v2"
-	port := "5432"
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, database, port)
-	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-
-	db, err := conn.DB()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	// Migrate
-	// Akan terbuat tabel otomatis
-	// Debug() -> Mengaktifkan log agar terlihat di balik layar
-	conn.Debug().AutoMigrate(&Vehicle{})
-
 }
---
+++ model/vehicle.go
@@ -0,0 +1,17 @@
+package model
+
+type Vehicle struct {
+	Brand          string `gorm:"varchar;size:30"`
+	Model          string `gorm:"varchar;size:30"`
+	ProductionYear int    `gorm:"size:4"`
+	Color          string `gorm:"varchar;size:30"`
+	IsAutomatic    bool   `gorm:"default:true"`
+	Stock          int    `gorm:"check:stock >= 0"`
+	SalePrice      int    `gorm:"check:sale_price > 0"`
+	Status         string `gorm:"check:status IN ('Baru', 'Bekas')"`
+	BaseModel
+}
+
+func (v *Vehicle) IsValidStatus() bool {
+	return v.Status == "Baru" || v.Status == "Bekas"
+}
