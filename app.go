85596a9c1b8bdde3feeb6c13c2b2a4dd0e9b7c91
jutionck <jutionck@gmail.com>
2023-03-25 01:39:41 +0700

    add: Config file

--- app.go
+++ app.go
@@ -3,27 +3,19 @@ package main
 import (
 	"fmt"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jutionck/golang-db-sinar-harapan-makmur-orm/config"
 )
 
 func main() {
 	fmt.Println("Accessing Database")
-	// Koneksi DB
-	host := "localhost"
-	user := "postgres"
-	password := "P@ssw0rd"
-	database := "db_sinar_harapan_makmur_v2"
-	port := "5432"
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, database, port)
-	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Get Connection DB From Config
+
+	c, err := config.NewConfig()
 	if err != nil {
-		panic(err)
+		fmt.Println(err, "", 102)
+		return
 	}
 
-	db, err := conn.DB()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	config.NewDbConnection(c)
+
 }
