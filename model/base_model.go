3096e7e70e26fd417b08b4b58a466fb8e4bd065c
jutionck <jutionck@gmail.com>
2023-03-25 23:45:49 +0700

    update: Base Model

--- model/base_model.go
+++ model/base_model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type BaseModel struct {
-	ID        string `gorm:"primaryKey"`
+	ID        string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
