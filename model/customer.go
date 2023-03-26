ba7dd93853d97ca8235e35cfe599ba86d6e296a0
jutionck <jutionck@gmail.com>
2023-03-26 22:18:36 +0700

    add: Topic - Belongs To

---
+++ model/customer.go
@@ -0,0 +1,19 @@
+package model
+
+import "time"
+
+type Customer struct {
+	BaseModel
+	FirstName        string `gorm:"size:30"`
+	LastName         string `gorm:"size:30"`
+	Address          string
+	Email            string `gorm:"unique;size:30"`
+	PhoneNumber      string `gorm:"unique;size:15"`
+	Bod              time.Time
+	UserCredentialID string
+	UserCredential   UserCredential
+}
+
+func (Customer) TableName() string {
+	return "mst_customer"
+}
