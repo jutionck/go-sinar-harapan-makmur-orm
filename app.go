ade77ffe74dbdccacd858d0e6903657a3f85fcf1
jutionck <jutionck@gmail.com>
2023-03-30 06:53:18 +0700

    add: Password Hash

--- app.go
+++ app.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 
 	"github.com/jutionck/golang-db-sinar-harapan-makmur-orm/config"
-	"github.com/jutionck/golang-db-sinar-harapan-makmur-orm/model"
+	"github.com/jutionck/golang-db-sinar-harapan-makmur-orm/repository"
+	"github.com/jutionck/golang-db-sinar-harapan-makmur-orm/usecase"
 )
 
 func main() {
@@ -14,7 +15,15 @@ func main() {
 	}
 
 	dbConn, _ := config.NewDbConnection(c)
-	// db := dbConn.Conn()
+	db := dbConn.Conn()
 
-	dbConn.Migrate(&model.Customer{}, &model.UserCredential{})
+	ur := repository.NewUserRepository(db)
+	uuc := usecase.NewAuthenticationUseCase(ur)
+
+	user, err := uuc.Login("admdin", "P@ssw0rd")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Selamat datang:", user.UserName)
 }
